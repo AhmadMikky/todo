fef461be2ba89df78bfee5548313dceb051d134e
AhmadMikky <amm00081@mix.wvu.edu>
2023-11-23 18:58:15 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ImgHero from "./images/images.jpg"; // Adjust the path accordingly
+import "./App.css"; // Import the CSS file
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+
+  const addTodo = () => {
+    const newTodo = {
+      title: todo,
+      done: false,
+      id: Math.random().toString(),
+    };
+    setTodos([...todos, newTodo]);
+    setTodo("");
+  };
+
+  const deleteItem = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="hero"
+      style={{
+        backgroundImage: `url(${ImgHero})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+        boxShadow: "inset 0 0 0 2000px rgba(0, 0, 0, 0.5)",
+      }}
+    >
+       
+      <div className="container">
+        <div className="form">
+          <input
+            className="input"
+            placeholder="Add New ToDo"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <button onClick={addTodo} className="addButton">
+            Add
+          </button>
+        </div>
+
+        <div className="todoListContainer">
+        {todos.map((item, index) => (
+  <div
+    key={item.id}
+    style={{
+      backgroundColor: index % 2 === 0 ? "#e0e0e0" : "#f0f0f0",
+    }}
+    className="todoContainer"  // Update class name
+  >
+    <div className="todo">
+      <span>{item.title}</span>
+    </div>
+    <button onClick={() => deleteItem(item.id)} className="deleteButton">  {/* Update class name */}
+      Delete
+    </button>
+  </div>
+))}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
